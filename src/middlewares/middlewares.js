7229ec26ce84f8d0e39dc3ce2d0e16822710f2d5
cabrera.luc <cabreralucaspatricio@gmail.com>
2022-12-21 19:09:17 -0300

    Cambios getPokemons

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -1,168 +1,138 @@
+const fetch = require("node-fetch");
+const { Pokemon } = require("../db");
+const { Deleted } = require("../db");
+const { all } = require("../routes/Pokemons");
 
-const fetch  = require("node-fetch")
-const {Pokemon} = require("../db")
-const {Deleted} = require("../db")
-const { all } = require("../routes/Pokemons")
-
-
-
-async function traerPokemons(){
-    const PokemonesDesdeBd = await Pokemon.findAll()
-    return PokemonesDesdeBd
+async function traerPokemons() {
+  const PokemonesDesdeBd = await Pokemon.findAll();
+  return PokemonesDesdeBd;
 }
 
-pokecreate = async (objeto) =>{
-    const newPokemon = await Pokemon.create(objeto)
+pokecreate = async (objeto) => {
+  const newPokemon = await Pokemon.create(objeto);
+};
 
+async function SubirPokemonesABd() {
+  const a = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=40`);
+  const b = await a.json();
 
-}
-
-
-async function SubirPokemonesABd (){
-
-
-    const a = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=40`)
-    const b = await a.json()
-  
-
-       b.results.forEach( async (element)=> {
-        const busquedaIndividual = await Pokemon.findOne({where:{name:element.name}})
-        const busquedaIndividual2 = await Deleted.findOne({where:{name:element.name}})
-        if(!busquedaIndividual && !busquedaIndividual2){
-        const a = await fetch(element.url)
-        let b = await a.json()
-        let c = []
-        b.types.forEach(a => c.push(a.type.name))
-        let d = c.join().replace(",",", ")
-         b = {
-            name:b.name,
-           health:b.stats[0].base_stat,
-            attack:b.stats[1].base_stat,
-            defense:b.stats[2].base_stat,
-           speed:b.stats[5].base_stat,
-           height:b.height,
-           weight:b.weight,
-        pokeNumber:b.id,
-        type:d,
-        img:b.sprites.other["official-artwork"].front_default
-
-       
-       }
-       await pokecreate(b)}
-   
-       }
-     )
-
+  b.results.forEach(async (element) => {
+    const busquedaIndividual = await Pokemon.findOne({
+      where: { name: element.name },
+    });
+    const busquedaIndividual2 = await Deleted.findOne({
+      where: { name: element.name },
+    });
+    if (!busquedaIndividual && !busquedaIndividual2) {
+      const a = await fetch(element.url);
+      let b = await a.json();
+      let c = [];
+      b.types.forEach((a) => c.push(a.type.name));
+      let d = c.join().replace(",", ", ");
+      b = {
+        name: b.name,
+        health: b.stats[0].base_stat,
+        attack: b.stats[1].base_stat,
+        defense: b.stats[2].base_stat,
+        speed: b.stats[5].base_stat,
+        height: b.height,
+        weight: b.weight,
+        pokeNumber: b.id,
+        type: d,
+        img: b.sprites.other["official-artwork"].front_default,
+      };
+      await pokecreate(b);
     }
- 
+  });
+}
 
 module.exports = {
-getPokemonsById : async (id)=>{
-
-    
-    
+  getPokemonsById: async (id) => {
     try {
-        
-        const pokemonQueBusco = await Pokemon.findByPk(id)
-        return pokemonQueBusco
-
+      const pokemonQueBusco = await Pokemon.findByPk(id);
+      return pokemonQueBusco;
     } catch (error) {
-        return {message: "Pokemon no encontrado"}
-        
+      return { message: "Pokemon no encontrado" };
     }
-   
-    
+  },
 
-},
+  getPokemons: async () => {
+    const PokemonsFromDB = await Pokemon.findAll();
+    const PokemonsFromApi = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=40`
+    );
+    const PfaJson = await PokemonsFromApi.json();
+    let PokemonsFromApiChanged = [];
 
-getPokemons: async ()=>{
+    PfaJson.results.forEach(async (pokemon) => {
+      const data = await fetch(pokemon.url);
+      let dataJson = await data.json();
+      let typesArray = [];
+      b.types.forEach((element) => typesArray.push(element.type.name));
+      let typesJoined = typesArray.join().replace(",", ", ");
+      dataTransformed = {
+        name: dataJson.name,
+        health: dataJson.stats[0].base_stat,
+        attack: dataJson.stats[1].base_stat,
+        defense: dataJson.stats[2].base_stat,
+        speed: dataJson.stats[5].base_stat,
+        height: dataJson.height,
+        weight: dataJson.weight,
+        pokeNumber: dataJson.id,
+        type: typesJoined,
+        img: dataJson.sprites.other["official-artwork"].front_default,
+      };
+      PokemonsFromApiChanged.push(dataTransformed)
+    });
 
-
-    const PokemonsFromDB = await Pokemon.findAll()        
-    const PokemonsFromApi = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=40`)
-    const PfaJson = await PokemonsFromApi.json()
-    const SumaDeAmbos=[...PfaJson.results, ...PokemonsFromDB]
+    const SumaDeAmbos = [...PokemonsFromApiChanged, ...PokemonsFromDB];
     return SumaDeAmbos
-   
+  },
+  getPokemonsFromTrash: async () => {
+    const pokemonsFromTrash = await Deleted.findAll();
+    return pokemonsFromTrash;
+  },
+  restorePokemons: async () => {
+    const a = await SubirPokemonesABd();
 
+    while (40 !== (await Pokemon.count())) {}
+    return traerPokemons();
+  },
 
-},
-getPokemonsFromTrash: async ()=>{
+  restoreFromTrash: async (lista) => {
+    lista.forEach(async (element) => {
+      const pokemonEnCuestion = await Deleted.findOne({
+        where: { name: element },
+      });
+      console.log(element);
+      while (!pokemonEnCuestion) {}
+      Deleted.destroy({ where: { name: element } });
+      const pokemonEnCuestionCreate = await Pokemon.create(
+        pokemonEnCuestion.dataValues
+      );
+    });
+  },
 
+  getPokemonsByName: async (name) => {
+    const pokemonQueBusco = await Pokemon.findOne({ where: { name } });
 
-    const pokemonsFromTrash = await Deleted.findAll()
-    return pokemonsFromTrash
-
-    
-   
-   
-
-
-},
-restorePokemons: async ()=>{
-
-
-   
-        const a= await SubirPokemonesABd()
-        
-        while (40!==await Pokemon.count()) {
-            
-        }
-           return traerPokemons()
-
-},
-
-restoreFromTrash: async (lista)=>{
-
-    lista.forEach( async (element) => {
-        const pokemonEnCuestion = await Deleted.findOne({where:{name:element}})
-        console.log(element)
-        while (!pokemonEnCuestion) {
-         
-        }
-        Deleted.destroy({where:{name:element}})
-        const pokemonEnCuestionCreate = await Pokemon.create(pokemonEnCuestion.dataValues)
-    })
-
-   
-    
-
-},
-
- getPokemonsByName : async (name)=>{
-
-  
-        
-        const pokemonQueBusco = await Pokemon.findOne({where:{name}})
-
-        if(pokemonQueBusco) return pokemonQueBusco
-        else{
-          
-        return error
-        
-        }
-
-   
-        
-    
-   
-    
-},
-createNewPokemon : async (objeto) =>{
+    if (pokemonQueBusco) return pokemonQueBusco;
+    else {
+      return error;
+    }
+  },
+  createNewPokemon: async (objeto) => {
     let re = /,/gi;
-    objeto.type = objeto.type.join().replace(re,", ")
-    objeto.health = Number(objeto.health)
-    objeto.attack = Number(objeto.attack)
-    objeto.defense = Number(objeto.defense)
-    objeto.speed = Number(objeto.speed)
-    objeto.height = Number(objeto.height)
-    objeto.weight = Number(objeto.weight)
-    const newPokemon = await Pokemon.create(objeto)
-
-
-},
-sendPokemonToTrash: async (objeto)=>{
-    const newPokemon = await Deleted.create(objeto)
-}
-
-}
+    objeto.type = objeto.type.join().replace(re, ", ");
+    objeto.health = Number(objeto.health);
+    objeto.attack = Number(objeto.attack);
+    objeto.defense = Number(objeto.defense);
+    objeto.speed = Number(objeto.speed);
+    objeto.height = Number(objeto.height);
+    objeto.weight = Number(objeto.weight);
+    const newPokemon = await Pokemon.create(objeto);
+  },
+  sendPokemonToTrash: async (objeto) => {
+    const newPokemon = await Deleted.create(objeto);
+  },
+};
